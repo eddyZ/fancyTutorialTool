8e52222ecd026052333c11c509920efa383c0bc6
stefan judis <stefanjudis@gmail.com>
2012-11-16 16:33:32 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FancyTutorialTool::Application.config.secret_token = '7d58d3ff580f1505593a0d36abacee8f53a1a7f5405fcada3612e379b5595adb9ceaf3670123ab8d977f18b7b34b8a069d5804ed8f6b531dc21dcba132253e29'
